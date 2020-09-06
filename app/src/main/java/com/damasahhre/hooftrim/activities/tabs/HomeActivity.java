30068796556613658616be22546ddea5aaaaac42
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-09-06 12:50:35 +0430

    designed home and next visit

--- app/src/main/java/com/damasahhre/hooftrim/activities/tabs/HomeActivity.java
+++ app/src/main/java/com/damasahhre/hooftrim/activities/tabs/HomeActivity.java
@@ -1,26 +1,29 @@
 package com.damasahhre.hooftrim.activities.tabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.damasahhre.hooftrim.R;
 import com.damasahhre.hooftrim.activities.MainActivity;
-
-import java.util.Objects;
+import com.damasahhre.hooftrim.activities.tabs.home_activites.VisitActivity;
 
 public class HomeActivity extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.activity_home, container, false);
+        View view = inflater.inflate(R.layout.activity_home, container, false);
         view.findViewById(R.id.menu_button).setOnClickListener(v -> {
             ((MainActivity) requireActivity()).openMenu();
+        });
+        view.findViewById(R.id.show_more).setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), VisitActivity.class);
+            startActivity(intent);
         });
         return view;
     }
