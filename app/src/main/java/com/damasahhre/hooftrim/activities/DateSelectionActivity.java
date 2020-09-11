d9b81b2b82e5bdd2b4ac29bf3baf1437f7ae8f58
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-09-11 21:24:19 +0430

    designed search cow

---
+++ app/src/main/java/com/damasahhre/hooftrim/activities/DateSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.damasahhre.hooftrim.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.damasahhre.hooftrim.R;
+
+public class DateSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_selection);
+    }
+}
