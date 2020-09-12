0a818b7866dd7f6f35df5d26f7d60f18d602cda9
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-09-12 10:27:08 +0430

    designed search livestrock

---
+++ app/src/main/java/com/damasahhre/hooftrim/activities/LiveStrockSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.damasahhre.hooftrim.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.damasahhre.hooftrim.R;
+
+public class LiveStrockSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_strock_selection);
+    }
+}
