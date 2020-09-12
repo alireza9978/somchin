a937e952aa731a925bc83a0dcca38bd1473dfd82
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-09-12 12:04:30 +0430

    designed add report step_1

---
+++ app/src/main/java/com/damasahhre/hooftrim/activities/reports/fragments/CowInfoFragment.java
@@ -0,0 +1,20 @@
+package com.damasahhre.hooftrim.activities.reports.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.damasahhre.hooftrim.R;
+
+public class CowInfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cow_info, container, false);
+    }
+}
