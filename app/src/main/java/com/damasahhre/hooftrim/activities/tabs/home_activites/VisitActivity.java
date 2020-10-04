aea7d39d293e31fd29fe95b6b0e72a58eb62f86e
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-10-04 19:41:11 +0330

    all visit list done

--- app/src/main/java/com/damasahhre/hooftrim/activities/tabs/home_activites/VisitActivity.java
+++ app/src/main/java/com/damasahhre/hooftrim/activities/tabs/home_activites/VisitActivity.java
@@ -1,10 +1,23 @@
 package com.damasahhre.hooftrim.activities.tabs.home_activites;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.damasahhre.hooftrim.R;
+import com.damasahhre.hooftrim.adapters.RecyclerViewAdapterAllNextVisit;
+import com.damasahhre.hooftrim.constants.Constants;
+import com.damasahhre.hooftrim.database.DataBase;
+import com.damasahhre.hooftrim.database.dao.MyDao;
+import com.damasahhre.hooftrim.database.models.NextReport;
+import com.damasahhre.hooftrim.database.utils.AppExecutors;
+import com.damasahhre.hooftrim.models.MyDate;
+
+import java.util.Date;
+import java.util.List;
 
 public class VisitActivity extends AppCompatActivity {
 
@@ -12,5 +25,21 @@ public class VisitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visit);
+
+        ImageView back = findViewById(R.id.back);
+        back.setOnClickListener(view -> finish());
+        Constants.setImageBack(this, back);
+
+        RecyclerView listView = findViewById(R.id.all_visit_lists);
+
+        MyDao dao = DataBase.getInstance(this).dao();
+        AppExecutors.getInstance().diskIO().execute(() -> {
+            List<NextReport> reports = dao.getAllNextVisit(new MyDate(new Date()));
+            RecyclerViewAdapterAllNextVisit mAdapter = new RecyclerViewAdapterAllNextVisit(reports, this);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            listView.setLayoutManager(layoutManager);
+            listView.setAdapter(mAdapter);
+        });
+
     }
 }
---
+++ app/src/main/java/com/damasahhre/hooftrim/adapters/RecyclerViewAdapterAllNextVisit.java
@@ -0,0 +1,80 @@
+package com.damasahhre.hooftrim.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.damasahhre.hooftrim.R;
+import com.damasahhre.hooftrim.activities.CowProfileActivity;
+import com.damasahhre.hooftrim.constants.Constants;
+import com.damasahhre.hooftrim.database.models.NextReport;
+
+import java.util.List;
+
+public class RecyclerViewAdapterAllNextVisit extends RecyclerView.Adapter<RecyclerViewAdapterAllNextVisit.Holder> {
+
+    private List<NextReport> nextReports;
+    private Context context;
+
+    public RecyclerViewAdapterAllNextVisit(List<NextReport> nextReports, Context context) {
+        this.nextReports = nextReports;
+        this.context = context;
+    }
+
+    public void setNextReports(List<NextReport> nextReports) {
+        this.nextReports = nextReports;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new Holder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.next_visit_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        NextReport report = nextReports.get(position);
+        holder.cowName.setText(R.string.visit_cow);
+        holder.cowName.append(" " + report.cowNumber);
+        holder.farmName.setText(report.farmName);
+        if (report.nextVisitDate.isToday()){
+            holder.date.setText(R.string.today);
+            holder.date.setTextColor(context.getResources().getColor(R.color.red));
+        }else {
+            holder.date.setText(report.nextVisitDate.toString());
+            holder.date.setTextColor(context.getResources().getColor(R.color.black));
+        }
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, CowProfileActivity.class);
+            intent.putExtra(Constants.COW_ID, report.cowId);
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return Math.min(nextReports.size(), 3);
+    }
+
+    static class Holder extends RecyclerView.ViewHolder {
+
+        TextView cowName;
+        TextView farmName;
+        TextView date;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            farmName = itemView.findViewById(R.id.cow_count_text);
+            cowName = itemView.findViewById(R.id.cattel_id);
+            date = itemView.findViewById(R.id.date_string);
+        }
+    }
+
+}
