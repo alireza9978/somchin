a32db8a11d039b9a7eb87f489ca0b2c67d2e1418
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-10-13 15:55:50 +0330

    write and share excel file

--- app/src/main/java/com/damasahhre/hooftrim/activities/FarmProfileActivity.java
+++ app/src/main/java/com/damasahhre/hooftrim/activities/FarmProfileActivity.java
@@ -1,6 +1,10 @@
 package com.damasahhre.hooftrim.activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
@@ -8,7 +12,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,10 +32,21 @@ import com.damasahhre.hooftrim.database.models.NextVisit;
 import com.damasahhre.hooftrim.database.utils.AppExecutors;
 import com.damasahhre.hooftrim.models.MyDate;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 public class FarmProfileActivity extends AppCompatActivity {
 
@@ -67,6 +84,8 @@ public class FarmProfileActivity extends AppCompatActivity {
         nextVisitView.setLayoutManager(layoutManager);
         mAdapter = new RecyclerViewAdapterNextVisitFarmProfile(new ArrayList<>(), this);
         nextVisitView.setAdapter(mAdapter);
+
+        export();
 
     }
 
@@ -111,9 +130,9 @@ public class FarmProfileActivity extends AppCompatActivity {
             List<Cow> allCows = dao.getAllCowOfFarm(id);
             runOnUiThread(() -> {
                 main:
-                for(Cow cow : allCows){
-                    for (CowWithLastVisit cowWithLastVisit : cows){
-                        if (cow.getId().equals(cowWithLastVisit.getId())){
+                for (Cow cow : allCows) {
+                    for (CowWithLastVisit cowWithLastVisit : cows) {
+                        if (cow.getId().equals(cowWithLastVisit.getId())) {
                             continue main;
                         }
                     }
@@ -142,6 +161,84 @@ public class FarmProfileActivity extends AppCompatActivity {
     private void showVisit() {
         visitTitle.setVisibility(View.VISIBLE);
         nextVisitView.setVisibility(View.VISIBLE);
+    }
+
+    public void export() {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Sample sheet");
+        //todo export farm info to file
+        Map<String, Object[]> data = new HashMap<>();
+        data.put("1", new Object[]{"Emp No.", "Name", "Salary"});
+        data.put("2", new Object[]{1d, "John", 1500000d});
+        data.put("3", new Object[]{2d, "Sam", 800000d});
+        data.put("4", new Object[]{3d, "Dean", 700000d});
+
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset) {
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = data.get(key);
+            int cellnum = 0;
+            assert objArr != null;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof Date)
+                    cell.setCellValue((Date) obj);
+                else if (obj instanceof Boolean)
+                    cell.setCellValue((Boolean) obj);
+                else if (obj instanceof String)
+                    cell.setCellValue((String) obj);
+                else if (obj instanceof Double)
+                    cell.setCellValue((Double) obj);
+            }
+        }
+
+        try {
+
+            //todo replace with farm name
+            String storage = Environment.getExternalStorageDirectory().toString() + String.format("/%s.xls", "alireza");
+            File file = new File(storage);
+            if (file.exists()) {
+                if (file.delete()) {
+                    Log.i("TAG", "export: deleted ok");
+                } else {
+                    Log.i("TAG", "export: deleted fuck");
+                }
+            }
+            if (file.createNewFile()) {
+                FileOutputStream out = new FileOutputStream(file);
+                workbook.write(out);
+                out.close();
+                Log.i("TAG", "export: Excel written successfully..");
+            } else {
+                Log.i("TAG", "export: Excel written fuck..");
+            }
+
+            Uri uri;
+            if (Build.VERSION.SDK_INT < 24) {
+                uri = Uri.fromFile(file);
+            } else {
+                uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
+            }
+
+            Intent intent = ShareCompat.IntentBuilder.from(this)
+                    .setType("*/*")
+                    .setStream(uri)
+                    .setChooserTitle("Choose bar")
+                    .createChooserIntent()
+                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+//            Intent sendIntent = new Intent();
+//            sendIntent.setAction(Intent.ACTION_SEND);
+//            sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"+file.getAbsolutePath()));
+//            sendIntent.setType("*/*");
+//
+//            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(intent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
