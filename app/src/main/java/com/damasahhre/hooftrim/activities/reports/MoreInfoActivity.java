58ffbb43110e2be6e56b326761e989a4e20f3ad8
Alireza Rahimi <rahimialireza.az@gmail.com>
2020-10-17 18:44:48 +0330

    add more info pages

--- app/src/main/java/com/damasahhre/hooftrim/activities/reports/MoreInfoActivity.java
+++ app/src/main/java/com/damasahhre/hooftrim/activities/reports/MoreInfoActivity.java
@@ -1,6 +1,7 @@
 package com.damasahhre.hooftrim.activities.reports;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,17 +16,25 @@ public class MoreInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+        findViewById(R.id.close_image).setOnClickListener(view -> finish());
+        Constants.setImageBack(this, findViewById(R.id.close_image));
         State state = (State) Objects.requireNonNull(getIntent().getExtras()).get(Constants.MORE_INFO_STATE);
         assert state != null;
+
         switch (state) {
             case info:
+                findViewById(R.id.one).setVisibility(View.VISIBLE);
                 break;
             case reason:
+                findViewById(R.id.two).setVisibility(View.VISIBLE);
                 break;
             case injury:
+                findViewById(R.id.three).setVisibility(View.VISIBLE);
                 break;
             case moreInfo:
+                findViewById(R.id.four).setVisibility(View.VISIBLE);
                 break;
         }
+
     }
 }
