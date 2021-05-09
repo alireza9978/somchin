9ea84213cd0fd8e890f97d37987c51903b9783b6
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-05-09 13:22:46 +0430

    add some extra intent

--- app/src/main/java/com/damasahhre/hooftrim/activities/menu/ContactActivity.java
+++ app/src/main/java/com/damasahhre/hooftrim/activities/menu/ContactActivity.java
@@ -40,8 +40,8 @@ public class ContactActivity extends AppCompatActivity {
             @Override
             public void onClick(@NonNull View view) {
                 Intent intent = new Intent(Intent.ACTION_SENDTO);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_EMAIL, "herdhealther@gmail.com");
+                String mailto = "mailto:herdhealther@gmail.com";
+                intent.setData(Uri.parse(mailto));
                 startActivity(intent);
             }
 
@@ -84,7 +84,19 @@ public class ContactActivity extends AppCompatActivity {
         }, 0, textPart.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
         ((TextView) findViewById(R.id.instagram_text)).setText(textPart);
         ((TextView) findViewById(R.id.instagram_text)).setMovementMethod(LinkMovementMethod.getInstance());
+        findViewById(R.id.instagram_icon).setOnClickListener(v -> {
+            Uri uri = Uri.parse("http://instagram.com/_u/hhre_damasa");
+            Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
 
+            likeIng.setPackage("com.instagram.android");
+
+            try {
+                startActivity(likeIng);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://instagram.com/hhre_damasa")));
+            }
+        });
 
         textPart = new SpannableString("@hhre_damasa");
         textPart.setSpan(new ClickableSpan() {
@@ -103,6 +115,10 @@ public class ContactActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.telegram_text)).setText(textPart);
         ((TextView) findViewById(R.id.telegram_text)).setMovementMethod(LinkMovementMethod.getInstance());
 
+        findViewById(R.id.telegram_icon).setOnClickListener(v -> {
+            Intent telegram = new Intent(Intent.ACTION_VIEW, Uri.parse("https://telegram.me/hhre_damasa"));
+            startActivity(telegram);
+        });
 
         textPart = new SpannableString("+989918760698");
         textPart.setSpan(new ClickableSpan() {
@@ -120,6 +136,12 @@ public class ContactActivity extends AppCompatActivity {
         }, 0, textPart.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
         ((TextView) findViewById(R.id.call_text)).setText(textPart);
         ((TextView) findViewById(R.id.call_text)).setMovementMethod(LinkMovementMethod.getInstance());
+
+        findViewById(R.id.call_icon).setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "+989918760698"));
+            startActivity(intent);
+        });
+
 
         textPart = new SpannableString("damasahhre.com");
         textPart.setSpan(new ClickableSpan() {
@@ -140,6 +162,13 @@ public class ContactActivity extends AppCompatActivity {
         }, 0, textPart.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
         ((TextView) findViewById(R.id.email_text)).setText(textPart);
         ((TextView) findViewById(R.id.email_text)).setMovementMethod(LinkMovementMethod.getInstance());
+
+        findViewById(R.id.email_icon).setOnClickListener(v -> {
+            String url = "http://www.damasahhre.com";
+            Intent inetent = new Intent(Intent.ACTION_VIEW);
+            inetent.setData(Uri.parse(url));
+            startActivity(inetent);
+        });
 
     }
 }
